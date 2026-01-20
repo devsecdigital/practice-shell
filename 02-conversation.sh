deec3436cfa167c6d6dba01737c7629ec6f91652
shaik hameed <shaikhameedbasha085@gmail.com>
2026-01-20 14:12:10 +0530

    git push

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "trump:: hey musk, how are you?"
+echo "msuk:: hello trump, i am fine. how are you doing"
+echo "trump:: i am fine too . whats up?"
+echo "msuk:: nothing, just going to mars now, will you come?"
+echo "trump:: sorry, you carry on! i will come once you come back"
+echo "hello:: all"
---
+++ 03-varibles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=TRUMP
+PERSON2=PERSON2
+
+echo "PERSON1:: hey PERSON2, how are you?"
+echo "PERSON2:: hello PERSON1, i am fine. how are you doing"
+echo "PERSON1:: i am fine too . whats up?"
+echo "PERSON2:: nothing, just going to mars now, will you come?"
+echo "PERSON1:: sorry, you carry on! i will come once you come back"
+
