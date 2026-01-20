4c476aaca7c915904feafce5a932f79e62ca0a7a
shaik hameed <shaikhameedbasha085@gmail.com>
2026-01-20 15:00:12 +0530

    hello world started

--- 03-varibles.sh
+++ 03-varibles.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-PERSON1=TRUMP
+PERSON1=trump
 PERSON2=musk
 
-echo "PERSON1:: hey PERSON2, how are you?"
-echo "PERSON2:: hello PERSON1, i am fine. how are you doing"
-echo "PERSON1:: i am fine too . whats up?"
-echo "PERSON2:: nothing, just going to mars now, will you come?"
-echo "PERSON1:: sorry, you carry on! i will come once you come back"
+echo "$PERSON1:: hey $PERSON2, how are you?"
+echo "$PERSON2:: hello $PERSON1, i am fine. how are you doing"
+echo "$PERSON1:: i am fine too . whats up?"
+echo "$PERSON2:: nothing, just going to mars now, will you come?"
+echo "$PERSON1:: sorry, you carry on! i will come once you come back"
 
