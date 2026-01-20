355321d40775c366ca5ecd15ded296790c9d060c
shaik hameed <shaikhameedbasha085@gmail.com>
2026-01-20 14:30:20 +0530

    hello world started

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "trump:: hey musk, how are you?"
-echo "msuk:: hello trump, i am fine. how are you doing"
-echo "trump:: i am fine too . whats up?"
-echo "msuk:: nothing, just going to mars now, will you come?"
-echo "trump:: sorry, you carry on! i will come once you come back"
+#echo "msuk:: hello trump, i am fine. how are you doing"
+##echo "trump:: i am fine too . whats up?"
+##echo "msuk:: nothing, just going to mars now, will you come?"
+##echo "trump:: sorry, you carry on! i will come once you come back"
