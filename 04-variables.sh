6b6e8ac616b73ac4720f75d31ca257692c03e3ed
shaik hameed <shaikhameedbasha085@gmail.com>
2026-01-20 15:07:18 +0530

    hello world started

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hey $PERSON2, how are you?"
+echo "$PERSON2:: hello $PERSON1, i am fine. how are you doing"
+echo "$PERSON1:: i am fine too . whats up?"
+echo "$PERSON2:: nothing, just going to mars now, will you come?"
+echo "$PERSON1:: sorry, you carry on! i will come once you come back"
