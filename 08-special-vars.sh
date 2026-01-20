de07a58356c78f703f7c2b2d1b2448b3f655165b
shaik hameed <shaikhameedbasha085@gmail.com>
2026-01-20 17:34:49 +0530

    special vars

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -3,3 +3,10 @@
 #echo "All Variables passed to the script: $@"
 echo "All Variables passed to the script: $@"
 echo  "number of variables: $#"
+echo "script name: $0"
+echo "current directory: $PWD"
+echo "user running this script: $USER"
+echo "home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in background: $!"
