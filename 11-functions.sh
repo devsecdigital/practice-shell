78c04ecde80602aa46a1b81632d7822af2593d01
shaik hameed <shaikhameedbasha085@gmail.com>
2026-01-22 10:45:06 +0530

    commands

---
+++ 11-functions.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR:: please run this script with root access"
+    exit 1 #give other than 0 upto 127
+else
+    echo "you are  running with root access"
+fi
+
+VALIDATE()
+      if [ $1 -eq 0 ]
+    then
+        echo "installing $2 is .... SUCCESS"
+    else 
+        echo "installing $2 is .... FAILURE"
+        exit 1
+    fi  
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "MYSQL is not installed... going to install it"
+    dnf install mysql -y
+    VALIDATE $? "MYSQL"
+else
+    echo "MYSQL is already installed...nothing to do"
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed... going to install it"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo "python3 is already installed...nothing to do"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    echo "nginx is not installed... going to install it"
+    dnf install nginx -y
+    VALIDATE $? "nginx"
+else
+    echo "nginx is already installed...nothing to do"
+fi
